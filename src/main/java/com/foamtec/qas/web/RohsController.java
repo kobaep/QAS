3a63d3c75fc130f5aa57cef0ae85f02f3c61719d
apichat <apichat.kop@gmail.com>
2015-06-28 20:48:58 +0700

    implement rohs page

--- src/main/java/com/foamtec/qas/web/RohsController.java
+++ src/main/java/com/foamtec/qas/web/RohsController.java
@@ -4,10 +4,17 @@ import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
+import org.springframework.ui.Model;
 
 @RooWebJson(jsonObject = Rohs.class)
 @Controller
 @RequestMapping("/rohses")
 @RooWebScaffold(path = "rohses", formBackingObject = Rohs.class)
 public class RohsController {
+
+	@RequestMapping(value = "/create", produces = "text/html")
+    public String bootstrapCreate(Model uiModel) {
+        return "RoHs/create";
+    }
+
 }
