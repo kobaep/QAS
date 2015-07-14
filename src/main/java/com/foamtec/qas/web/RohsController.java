e240323106879ec020bd9d8c472147f47a8b29c1
apichat <apichat.kop@gmail.com>
2015-07-14 16:33:39 +0700

    update upload file 3 item

--- src/main/java/com/foamtec/qas/web/RohsController.java
+++ src/main/java/com/foamtec/qas/web/RohsController.java
@@ -33,12 +33,32 @@ public class RohsController {
     public @ResponseBody ResponseEntity<String> pdfSpecImport(MultipartRequest multipartrequest, HttpServletResponse response, Model uiModel) throws IOException {
     	HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Type", "application/json");
-        MultipartFile file   =  multipartrequest.getFile("specFile");
-        File convFile = new File("../fileUpload/"+file.getOriginalFilename());
-        convFile.createNewFile(); 
-	    FileOutputStream fos = new FileOutputStream(convFile);
-	    fos.write(file.getBytes());
-	    fos.close();
-        return new ResponseEntity<String>("Duplicated File Name", headers, HttpStatus.OK);
+        boolean specFile = uploadFile(multipartrequest, "specFile");
+        boolean msdsFile = uploadFile(multipartrequest, "msdsFile");
+        boolean rohsFile = uploadFile(multipartrequest, "rohsFile");
+        if (specFile && msdsFile && rohsFile) {
+            return new ResponseEntity<String>("finish", headers, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<String>("error", headers, HttpStatus.OK);
+        } 
+    }
+
+    private boolean uploadFile(MultipartRequest multipartrequest, String name) {
+        try {
+            if (multipartrequest.getFile(name) == null) {
+                return true;
+            } else {
+                MultipartFile file   =  multipartrequest.getFile(name);
+                String pathSpecFile = "./src/main/webapp/fileUploads/" + name + "/";
+                File convFile = new File(pathSpecFile+file.getOriginalFilename());
+                convFile.createNewFile(); 
+                FileOutputStream fos = new FileOutputStream(convFile);
+                fos.write(file.getBytes());
+                fos.close();
+                return true;
+            }
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
